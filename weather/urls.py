7045d8c0e2bc486ca326b52beb6a7306f0a829ba
VIGNESHS SRI S <vigneshsri3535@gmail.com>
2025-09-18 22:56:56 +0530

    Add files via upload

--- weather/urls.py
+++ weather/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('upload/', views.upload_csv, name='upload_csv'),
 ]
